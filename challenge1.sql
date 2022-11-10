123de0a8929e03e1f647a8197015df6dac60eacb
maxellende <115661530+maxellende@users.noreply.github.com>
2022-11-10 09:10:33 +0100

    up ch1

--- challenge1.sql
+++ challenge1.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(*) as ' total number of references not tracked in the app but present in the shop assortment' FROM 'retailer' WHERE 'retailer'.'Stockenquantité' > 0 AND 'retailer'.EAN NOT IN (SELECT reference_id FROM 'references');
+SELECT COUNT(*) as ' total number of references not tracked in the app but present in the shop assortment' FROM 'retailer' WHERE 'retailer'.EAN NOT IN (SELECT reference_id FROM 'references');
